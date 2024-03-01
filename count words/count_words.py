bc685f910863a5862fa2f24be972d3f9c8bdd1ef
Abdoosaeid <abdosaid2342004@gmail.com>
2024-03-01 15:34:06 +0200

    pretty print Function

--- count words/count_words.py
+++ count words/count_words.py
@@ -16,6 +16,16 @@ def process_line(line="",word_count_dict={}):
             word=word.strip(string.punctuation)
             add_word(word,word_count_dict)
 
+def pretty_print(word_count_dict={}):
 
+    value_key_list=[]
+    for key,val in word_count_dict.items():
+        value_key_list.append((val,key))
 
-            
+    value_key_list.sort(reverse=True)
+
+    print('{:11s}{:11s}'.format('Word','Count'))
+    print('_'*21)
+    for val,key in value_key_list:
+        print('{:12s}{:<3d}'.format(key,val))
+                        
