6a2950bf36067070478d71aa50823cb749973d07
Abdoosaeid <abdosaid2342004@gmail.com>
2024-03-01 15:20:28 +0200

    process line function

--- count words/count_words.py
+++ count words/count_words.py
@@ -3,3 +3,19 @@ def add_word(word,word_count_dict):
         word_count_dict[word]+=1
     else:
         word_count_dict[word]=1    
+
+import string
+def process_line(line="",word_count_dict={}):
+    line=line.strip()
+    word_list=line.split()
+    for word in word_list:
+        if word !='__':
+            word=word.lower()
+            word=word.strip() 
+
+            word=word.strip(string.punctuation)
+            add_word(word,word_count_dict)
+
+
+
+            
