3fd626d2d29d3b4e66ac6b9e3246ceeb011705b7
Abdoosaeid <abdosaid2342004@gmail.com>
2024-03-12 08:37:15 +0200

    add project

---
+++ RecursiveTree/RecursiveTree.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+def branch(length,level):
+    if level<=0:
+        return
+    forward(length)
+    left(45)
+    branch(0.6*length,level-1)
+    right(90)
+    branch(0.6*length,level-1)
+    left(45)
+    backward(length)
+    return
+
+left(90)
+branch(100,6)
+    
