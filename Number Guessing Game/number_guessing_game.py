2278067efb00af91acbc723412d7ef16a5295c66
Abdoosaeid <abdosaid2342004@gmail.com>
2024-03-15 14:15:21 +0200

    add project

---
+++ Number Guessing Game/number_guessing_game.py
@@ -0,0 +1,39 @@
+import random
+
+top_range = input("Type a number: ")
+
+if top_range.isdigit():
+    top_range=int(top_range)
+
+    if top_range<=0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else :
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0,top_range)
+guesses = 0
+
+while True:
+    guesses+=1
+    user_guess =input("Type your guess: ")
+    if user_guess.isdigit():
+        user_guess=int(user_guess)
+    else:
+        print("please type a number next time. ")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!") 
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
+
+
+
+
