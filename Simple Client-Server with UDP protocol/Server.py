3b1ffb7a19d30e4d0718eba294fb860d3a299918
Abdoosaeid <abdosaid2342004@gmail.com>
2024-07-08 02:50:39 +0300

    add update

--- Simple Client-Server with UDP protocol/Server.py
+++ Simple Client-Server with UDP protocol/Server.py
@@ -1,14 +1,16 @@
 import socket
 
-server_host = "localhost"
 server_port = 12000
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((server_host, server_port))
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-print("Server is ready to receive")
+server_socket.bind(('',server_port))
+print("The server is ready to receicve")
 
 while True:
-    message, client_address = server_socket.recvfrom(2048)
-    modified_message = message.decode('utf-8').upper()
-    server_socket.sendto(modified_message.encode('utf-8'), client_address)
+    data ,client_Address = server_socket.recvfrom(2048)
+    message = data.decode('UTF-8')
+    print(message)
+    new_message = message.upper()
+
+    server_socket.sendto(new_message.encode("UTF-8"),client_Address)
