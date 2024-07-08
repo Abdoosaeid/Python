25fc8cf650993d61b2f7a9cac4716e3c91660df7
Abdoosaeid <abdosaid2342004@gmail.com>
2024-07-08 03:21:37 +0300

    add a new column

--- Automation with python/app.py
+++ Automation with python/app.py
@@ -4,4 +4,10 @@ wb = xl.load_workbook("transactions.xlsx")
 
 sheet = wb["Sheet1"]
  
- 
+for row in range(2,sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value = corrected_price
+
+wb.save("transactions2.xlsx")
