b25360cde071288dee06407847e4b5a6d3633855
Abdoosaeid <abdosaid2342004@gmail.com>
2024-07-08 03:36:04 +0300

    add chart

--- Automation with python/app.py
+++ Automation with python/app.py
@@ -1,5 +1,5 @@
 import openpyxl as xl 
-
+from openpyxl.chart import BarChart,Reference
 wb = xl.load_workbook("transactions.xlsx")
 
 sheet = wb["Sheet1"]
@@ -9,5 +9,17 @@ for row in range(2,sheet.max_row + 1):
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row,4)
     corrected_price_cell.value = corrected_price
+
+valus = Reference(
+    sheet,
+    min_row=2,
+    max_row=4,
+    min_col=2,
+    max_col=4
+)
+
+chart = BarChart()
+chart.add_data(valus)
+sheet.add_chart(chart,"e2")
 
 wb.save("transactions2.xlsx")
